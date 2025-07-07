77bc839093ccaa0fe4d555b980ef5f7568afc35e
Ana-Maria Predescu <Ana-Maria.Predescu@softwire.com>
2025-07-07 17:07:40 +0300

    Red4 - Bishop moves

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
+import GameSettings from "../gameSettings";
 
 export default class Bishop extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,34 @@ export default class Bishop extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let newPosition: Square[] = [];
+        const currentPosition= board.findPiece(this);
+
+        for (let i = 0; i < currentPosition.row; i++) {
+            if (currentPosition.col - currentPosition.row + i >= 0) {
+                newPosition.push(new Square(i, currentPosition.col - currentPosition.row + i));
+            }
+        }
+
+        for (let i = currentPosition.row + 1; i < GameSettings.BOARD_SIZE; i++) {
+            if (currentPosition.col - currentPosition.row + i <= 7){
+                newPosition.push(new Square(i, currentPosition.col - currentPosition.row + i));
+            }
+        }
+
+        for (let j = GameSettings.BOARD_SIZE - 1; j > currentPosition.col; j--) {
+            if (currentPosition.col + currentPosition.row - j >= 0) {
+                newPosition.push(new Square(currentPosition.col + currentPosition.row - j, j));
+            }
+        }
+
+        for (let j = currentPosition.col - 1; j >= 0; j--) {
+            if (currentPosition.col + currentPosition.row - j >= 0) {
+                newPosition.push(new Square(currentPosition.col + currentPosition.row - j, j));
+            }
+        }
+
+
+        return newPosition;
     }
 }
