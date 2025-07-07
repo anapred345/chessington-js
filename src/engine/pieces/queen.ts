dffe16dea7f5d02567c0256faf560e6ffe27d893
Ana-Maria Predescu <Ana-Maria.Predescu@softwire.com>
2025-07-07 17:29:23 +0300

    Red5 - Queen Moves

--- src/engine/pieces/queen.ts
+++ src/engine/pieces/queen.ts
@@ -1,6 +1,9 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Bishop from "./bishop";
+import Rook from "./rook";
+import Square from "../square";
 
 export default class Queen extends Piece {
     public constructor(player: Player) {
@@ -8,6 +11,16 @@ export default class Queen extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currentPosition= board.findPiece(this);
+
+        const rook = new Rook(this.player);
+        board.setPiece(currentPosition, rook);
+        const rook_moves = rook.getAvailableMoves(board);
+
+        const bishop = new Bishop(this.player);
+        board.setPiece(currentPosition, bishop);
+        const bishop_moves = bishop.getAvailableMoves(board);
+
+        return [... rook_moves, ... bishop_moves];
     }
 }
